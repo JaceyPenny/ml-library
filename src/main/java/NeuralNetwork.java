67b7bc4829fc0fe20e892f23526e0e03e8ae75f7
Jace McPherson <jace-mcpherson@att.net>
2018-01-27 16:45:48 -0600

    Moved SSE and CV implementations to SupervisedLearner

--- src/main/java/NeuralNetwork.java
+++ src/main/java/NeuralNetwork.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class NeuralNetwork extends SupervisedLearner {
     private int layerSize;
@@ -58,61 +57,5 @@ public class NeuralNetwork extends SupervisedLearner {
 
         layer.activate(weights, in);
         return layer.activation;
-    }
-
-    @Override
-    double computeSumSquaredError(Matrix testFeatures, Matrix expectedLabels) {
-        double sumSquaredError = 0;
-
-        for (int i = 0; i < testFeatures.rows(); i++) {
-            Vector x_i = testFeatures.row(i);
-            Vector expected_y = expectedLabels.row(i);
-            Vector calculated_y = predict(x_i);
-
-            // Calculate squared error
-            calculated_y.scale(-1);
-            calculated_y.add(expected_y);
-            double squaredError = calculated_y.squaredMagnitude();
-
-            sumSquaredError += squaredError;
-        }
-
-        return sumSquaredError;
-    }
-
-    private void shuffleData(Matrix features, Matrix labels) {
-        Random random = new Random();
-        for (int i = features.rows(); i >= 2; i--) {
-            int r = random.nextInt(i);
-            features.swapRows(i - 1, r);
-            labels.swapRows(i - 1, r);
-        }
-    }
-
-    @Override
-    double crossValidation(int folds, int repetitions, Matrix features, Matrix labels) {
-        int dataRows = features.rows();
-        int[] foldSizes = Matrix.computeFoldSizes(dataRows, folds);
-
-        double totalError = 0;
-
-        for (int r = 0; r < repetitions; r++) {
-            shuffleData(features, labels);
-
-            for (int i = 0; i < folds; i++) {
-                Matrix X = Matrix.matrixWithoutFold(foldSizes, i, features);
-                Matrix Y = Matrix.matrixWithoutFold(foldSizes, i, labels);
-
-                Matrix test_X = Matrix.matrixFold(foldSizes, i, features);
-                Matrix expected_Y = Matrix.matrixFold(foldSizes, i, labels);
-
-                train(X, Y);
-
-                double sumSquaredError = computeSumSquaredError(test_X, expected_Y);
-                totalError += sumSquaredError;
-            }
-        }
-
-        return Math.sqrt(totalError / repetitions / features.rows());
     }
 }
--- src/main/java/SupervisedLearner.java
+++ src/main/java/SupervisedLearner.java
@@ -3,6 +3,8 @@
 // See http://creativecommons.org/publicdomain/zero/1.0/
 // ----------------------------------------------------------------
 
+import java.util.Random;
+
 abstract class SupervisedLearner {
     /**
      * Return the name of this learner
@@ -22,9 +24,59 @@ abstract class SupervisedLearner {
     /**
      * Computes the sum squared error for this learner.
      */
-    abstract double computeSumSquaredError(Matrix testFeatures, Matrix expectedLabels);
+    double computeSumSquaredError(Matrix testFeatures, Matrix expectedLabels) {
+        double sumSquaredError = 0;
 
-    abstract double crossValidation(int folds, int repetitions, Matrix features, Matrix labels);
+        for (int i = 0; i < testFeatures.rows(); i++) {
+            Vector x_i = testFeatures.row(i);
+            Vector expected_y = expectedLabels.row(i);
+            Vector calculated_y = predict(x_i);
+
+            // Calculate squared error
+            calculated_y.scale(-1);
+            calculated_y.add(expected_y);
+            double squaredError = calculated_y.squaredMagnitude();
+
+            sumSquaredError += squaredError;
+        }
+
+        return sumSquaredError;
+    }
+
+    private void shuffleData(Matrix features, Matrix labels) {
+        Random random = new Random();
+        for (int i = features.rows(); i >= 2; i--) {
+            int r = random.nextInt(i);
+            features.swapRows(i - 1, r);
+            labels.swapRows(i - 1, r);
+        }
+    }
+
+    double crossValidation(int folds, int repetitions, Matrix features, Matrix labels) {
+        int dataRows = features.rows();
+        int[] foldSizes = Matrix.computeFoldSizes(dataRows, folds);
+
+        double totalError = 0;
+
+        for (int r = 0; r < repetitions; r++) {
+            shuffleData(features, labels);
+
+            for (int i = 0; i < folds; i++) {
+                Matrix X = Matrix.matrixWithoutFold(foldSizes, i, features);
+                Matrix Y = Matrix.matrixWithoutFold(foldSizes, i, labels);
+
+                Matrix test_X = Matrix.matrixFold(foldSizes, i, features);
+                Matrix expected_Y = Matrix.matrixFold(foldSizes, i, labels);
+
+                train(X, Y);
+
+                double sumSquaredError = computeSumSquaredError(test_X, expected_Y);
+                totalError += sumSquaredError;
+            }
+        }
+
+        return Math.sqrt(totalError / repetitions / features.rows());
+    }
     /**
      * Measures the misclassificaions with the provided test data
      */
