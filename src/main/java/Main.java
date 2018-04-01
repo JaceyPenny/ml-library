1014ede02939de3dc7298d7c09c8ea845cd9ab6d
Jace McPherson <jace-mcpherson@att.net>
2018-04-01 18:55:03 -0500

    Variable names and improper "input" for assignment 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -266,8 +266,7 @@ class Main {
         3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0,
         4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8,
         4.9, 5.0, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6,
-        5.7, 5.8, 5.9, 6.0, 6.1, 6.2, 6.3, 6.4,
-        6.5, 6.6, 6.7, 6.8, 6.9, 7.0, 7.1, 7.2,
+        5.7, 5.8, 5.9, 6.0, 6.1, 6.2, 6.3, 6.4
     });
 
     Vector testOutput = new Vector(new double[]{
@@ -287,6 +286,6 @@ class Main {
   }
 
   public static void main(String[] args) {
-    runMNISTwithCNN();
+    runAssignment4();
   }
 }
